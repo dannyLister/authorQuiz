ff64c4e47b587518da598ba0f0717aec5aa02436
dannyLister <Daniel.Lister@coveainsurance.co.uk>
2019-01-03 13:52:30 +0000

    Bootstrap now works! logo removed, title placeholder

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Bootstrap from 
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
